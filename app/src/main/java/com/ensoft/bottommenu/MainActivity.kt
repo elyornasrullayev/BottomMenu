db922584c7a61a5a975b0df0f729e66a4c5469fa
elyor <elyor.nasrullayev1999yil@gmail.com>
2024-01-16 20:47:28 +0500

    Initial commit

---
+++ app/src/main/java/com/ensoft/bottommenu/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ensoft.bottommenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+    }
+}
